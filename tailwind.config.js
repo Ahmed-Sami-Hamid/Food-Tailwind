8b10ab343e51bc7ba65c651bfa6f36e9aafa6671
ahmed-saim-hamid <ahmedsamihamid1@gmail.com>
2022-01-26 05:12:53 +0200

    tailwind css restorant app

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#ff6363',
+        seconary:{
+          100:'#e2e2ds',
+          200:'#888883',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
